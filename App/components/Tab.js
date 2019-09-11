30a70bace21930f4d679b5059092f5b270874b82
DAH 11 <dah11@FORMATION.lan>
2019-09-11 14:04:36 +0200

    logo

--- App/components/Tab.js
+++ App/components/Tab.js
@@ -1,59 +1,65 @@
 import React, { Component } from 'react';
 import { Container, Header, Content, Tab, Tabs } from 'native-base';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 import DATA from '../consts/data';
 
 export default class Present extends Component {
   render() {
       let adress = this.props.adress;
     return (
-      <Container>
-        <Header hasTabs />
-        <Tabs>
-          <Tab heading="Infos">
-            <View >
-                    
+      
+        <Container>
+          <Header hasTabs />
+          <Tabs>
+            <Tab heading="Infos">
+              <View >           
+                        
+                              <View>
+                                  <Text >{adress.groupeparent}</Text>
+                                  <Text >{adress.societe}</Text>
+                                  <Text >{adress.typeBatiment}</Text>
+                                  <Text >{adress.description}</Text>
+                              </View>
+                        
+                        
+                  </View>
+              
+            </Tab>
+            <Tab heading="Coordonées Responsables">
+              
+                  <View>
                       
-                            <View>
-                                <Text >{adress.groupeparent}</Text>
-                                <Text >{adress.societe}</Text>
-                                <Text >{adress.typeBatiment}</Text>
-                                <Text >{adress.description}</Text>
-                            </View>
-                       
-                       
-                </View>
-            
-          </Tab>
-          <Tab heading="Coordonées Responsables">
-            
-                <View>
+                              <View>
+                                  <Text >{adress.prenom}</Text>
+                                  <Text >{adress.nom}</Text>
+                                  {/* <Text >{adress.}</Text> */}
+                              </View>
+                        
+                  </View>
+              
+            </Tab>
+            <Tab heading="Coordonées Entreprise">
+              
+              <View>
                     
-                            <View>
-                                <Text >{adress.prenom}</Text>
-                                <Text >{adress.nom}</Text>
-                                {/* <Text >{adress.}</Text> */}
-                            </View>
-                       
-                </View>
-            
-          </Tab>
-          <Tab heading="Coordonées Entreprise">
-            
-            <View>
-                   
-                            <View>
-                                <Text >{adress.tel}</Text>
-                                <Text >{adress.ville}</Text>
-                                <Text >{adress.rue}</Text>
-                                <Text >{adress.societe}</Text>
-                            </View>
-                       
-                </View>
-            
-          </Tab>
-        </Tabs>
-      </Container>
+                              <View>
+                                  <Text >{adress.tel}</Text>
+                                  <Text >{adress.ville}</Text>
+                                  <Text >{adress.rue}</Text>
+                                  <Text >{adress.societe}</Text>
+                              </View>
+                        
+                  </View>
+              
+            </Tab>
+          </Tabs>
+        </Container>
+      
+      
     );
   }
+}
+
+const style = StyleSheet {
+  
 }
