0d8f8091194e91bcc2a5a944ff384551dec7b6fd
DAH 11 <dah11@FORMATION.lan>
2019-09-13 14:53:30 +0200

    list

--- App/components/Liste.js
+++ App/components/Liste.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import {View, Text, ScrollView} from 'react-native';
 import { Container, Header, Content, List, ListItem, Thumbnail,  Left, Body, Right, Button } from 'native-base';
-
+ 
 
 export default class Liste extends Component {
     constructor(props){
@@ -12,21 +12,66 @@ export default class Liste extends Component {
     render(){
        
         return(
-            <ScrollView>
-                <View style={{flex:1}}>
-                    {
-                        this.props.adress.map((adress, i) =>{
-                            return (
-                            <View key={i}>
-                                <Text>{adress.societe}</Text>
-                            </View>
-                        )})
-                    }    
-                </View>
-                
-            </ScrollView>
+            <Container>
+                <ScrollView>
+                    <Content>
+                        <Header />
+                        <List>
+                            
+                                <View style={{flex:1}}>
+                                    {
+                                        this.props.adress.map((adress, i) =>{
+                                            return (
+                                                <View key={i}>
+                                                    <ListItem thumbnail>
+                                                        <Left>
+                                                            <Thumbnail square source={
+                                                                adress.logo
+                                                            } style = {{
+                                                                width: 60,
+                                                                height: 30,
+                                                                resizeMode: 'contain'
+                                                            }}/>
+                                                        </Left> 
+                                                    <Body>
+                                                    
+                                                        <Text>{adress.societe}</Text>
+                                                        <Text note numberOfLines={1}>{adress.ville}  {adress.typeBatiment}</Text>
+                                                    </Body>
+                                                        <Right>
+                                                            <Button transparent>
+                                                                <Text>Voir</Text>
+                                                            </Button>
+                                                        </Right>
+                                                    
+                                                    </ListItem>   
+                                
+                                                </View>
+                                            )
+                                        })
+                                    } 
+                                 </View>    
+                           
+                        </List>
+        
+                    </Content>
+                </ScrollView>
+            </Container>
         )
     }
 }
 
 
+/*
+        
+          
+            
+              
+              
+              
+            
+          
+      
+    );
+  }
+}*/
